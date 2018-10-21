0d3c16ca5d1f7cd213303649b93f243166c60552
João Duarte <onlyneplayer@gmail.com>
2018-10-21 11:39:23 +0100

    Added the class that will read from the file

---
+++ Steam DB/CSVParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Steam_DB {
+    public class CSVParser {
+        private StreamReader file;
+        private string line;
+
+        public void ReadCSVFile(IDictionary<int, Game> database, 
+            string filePath) {
+            file = File.OpenText(filePath);
+
+            while ((line = file.ReadLine()) != null) {
+                string[] fields = new string[24];
+                fields = line.Split(',');
+
+                //for a meter dentro da DB
+            }
+        }
+    }
+}
