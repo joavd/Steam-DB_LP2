b5b56292334a0dc376b9aff6cb092ffe33729aa1
João Duarte <onlyneplayer@gmail.com>
2018-10-22 18:32:50 +0100

    Fixed the parser to read the proper value

--- Steam DB/CSVParser.cs
+++ Steam DB/CSVParser.cs
@@ -6,64 +6,65 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 
-namespace Steam_DB
-{
-    public class CSVParser
-    {
+namespace Steam_DB {
+    public class CSVParser {
         private StreamReader file;
         private string line;
 
-        public void ReadCSVFile(HashSet<Game> database,
-            string filePath)
-        {
+        public void ReadCSVFile(ICollection<Game> database, string filePath) {
             file = File.OpenText(filePath);
 
-            while ((line = file.ReadLine()) != null)
-            {
+
+            while ((line = file.ReadLine()) != null) {
                 string[] fields = new string[24];
                 fields = line.Split(',');
-                if (fields[0] == "ID")
-                {
+                if (fields[0] == "ID") {
 
-                }
-                else
-                {
-                    Uri uri1, uri2,uri3;
-                    uri1 = new System.Uri(fields[21]);
-                    uri2 = new System.Uri(fields[23]);
-                    uri3 = new System.Uri(fields[24]);
+                } else {
+                    int numSup = 21, numImg = 23, numWeb = 24;
+                    Uri supportURL, image, website;
+                    int[] intSupport = new int[8];
 
-                    database.Add
-                    (
-                      new Game()
-                      {
-                          ID = Convert.ToInt32(fields[0]),
-                          Name = fields[1],
-                          ReleaseDate = Convert.ToDateTime(fields[2]),
-                          RequiredAge = Convert.ToInt32(fields[3]),
-                          DLCCount = Convert.ToInt32(fields[4]),
-                          Metacritic = Convert.ToInt32(fields[5]),
-                          MovieCount = Convert.ToInt32(fields[6]),
-                          RecommendationCount = Convert.ToInt32(fields[7]),
-                          ScreenshotCount = Convert.ToInt32(fields[8]),
-                          Owners = Convert.ToInt32(fields[9]),
-                          NumberOfPlayers = Convert.ToInt32(fields[10]),
-                          AchievementCount = Convert.ToInt32(fields[11]),
-                          ControllerSupport = Convert.ToBoolean(fields[12]),
-                          PlatformWindows = Convert.ToBoolean(fields[13]),
-                          PlatformLinux = Convert.ToBoolean(fields[14]),
-                          PlatformMac = Convert.ToBoolean(fields[15]),
-                          CategorySinglePlayer = Convert.ToBoolean(fields[16]),
-                          CategoryMultiplayer = Convert.ToBoolean(fields[17]),
-                          CategoryCoop = Convert.ToBoolean(fields[18]),
-                          CategoryIncludeLevelEditor = Convert.ToBoolean(fields[19]),
-                          CategoryVRSupport = Convert.ToBoolean(fields[20]),
-                          SupportURL = uri1,
-                          AboutText = fields[22],
-                          HeaderImage = uri2,
-                          Website = uri3
-                      }
-                    );
+                    for (int i = 0; i < 9; i++) {
+                        Int32.TryParse(fields[i+3], out int newInt);
+                        fields[i + 3] = newInt.ToString();
+                    }
+
+                    DateTime.TryParse(fields[2], out DateTime time);
+                    supportURL = (Uri.TryCreate(fields[numSup], 0, out Uri sup)) ?
+                        sup : null;
+                    image = (Uri.TryCreate(fields[numImg], 0, out Uri img)) ?
+                        img : null;
+                    website = (Uri.TryCreate(fields[numWeb], 0, out Uri web)) ?
+                        web : null;
+
+                    database.Add(new Game(
+                      Convert.ToInt32(fields[0]),
+                      fields[1],
+                      time,
+                      Convert.ToInt32(fields[3]),
+                      Convert.ToInt32(fields[4]),
+                      Convert.ToInt32(fields[5]),
+                      Convert.ToInt32(fields[6]),
+                      Convert.ToInt32(fields[7]),
+                      Convert.ToInt32(fields[8]),
+                      Convert.ToInt32(fields[9]),
+                      Convert.ToInt32(fields[10]),
+                      Convert.ToInt32(fields[11]),
+                      Convert.ToBoolean(fields[12]),
+                      Convert.ToBoolean(fields[13]),
+                      Convert.ToBoolean(fields[14]),
+                      Convert.ToBoolean(fields[15]),
+                      Convert.ToBoolean(fields[16]),
+                      Convert.ToBoolean(fields[17]),
+                      Convert.ToBoolean(fields[18]),
+                      Convert.ToBoolean(fields[19]),
+                      Convert.ToBoolean(fields[20]),
+                      supportURL,
+                      fields[22],
+                      image,
+                      website
+                      ));
                 }
             }
         }
